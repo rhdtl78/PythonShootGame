4d4260ced39171d76a7a4e491c1d9626dbd520ba
JunghoonK <rhdtl78@gamil.com>
2017-12-11 23:47:38 +0900

    시작 화면 구성 \n\n시작화면에서 시작과 종료 선택 가능

--- gameRole.py
+++ gameRole.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+﻿#-*- coding: utf-8 -*-
 """
 Created on Wed Sep 11 16:36:03 2013
 
@@ -6,7 +6,7 @@ Created on Wed Sep 11 16:36:03 2013
 """
 
 import pygame
-
+from pygame.locals import *
 SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 800
 
@@ -16,88 +16,161 @@ TYPE_BIG = 3
 
 # 글 머리 기호 클래스
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, bullet_img, init_pos):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = bullet_img
-        self.rect = self.image.get_rect()
-        self.rect.midbottom = init_pos
-        self.speed = 10
-
-    def move(self):
-        self.rect.top -= self.speed
+	def __init__(self, bullet_img, init_pos):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = bullet_img
+		self.rect = self.image.get_rect()
+		self.rect.midbottom = init_pos
+		self.speed = 10
+	
+	def move(self):
+		self.rect.top -= self.speed
 		
-    def enemy_move(self):
-        self.rect.top += 7
+	def enemy_move(self):
+		self.rect.top += 7
 
 # 플레이어 등급
 class Player(pygame.sprite.Sprite):
-    def __init__(self, plane_img, player_rect, init_pos):
-        pygame.sprite.Sprite.__init__(self)
-	# 플레이어 오브젝트 스프라이트 목록을 저장하는 데 사용됩니다.
-        self.image = []
-        for i in range(len(player_rect)):
-            self.image.append(plane_img.subsurface(player_rect[i]).convert_alpha())
-	# 그림의 사각형을 초기화하십시오.
-        self.rect = player_rect[0]
-	# 사각형의 왼쪽 위 모서리 좌표를 초기화합니다.
-        self.rect.topleft = init_pos
-	# 초기 플레이어 속도, 여기에 특정 값이 있습니다.
-        self.speed = 8
-	# 플레이어의 항공기에 의해 발사 된 총알의 컬렉션
-        self.bullets = pygame.sprite.Group()
-	# 플레이어 마법사 그림 색인
-        self.img_index = 0
-	# 플레이어가 맞았는지 여부
-        self.is_hit = False                             
+	def __init__(self, plane_img, player_rect, init_pos):
+		pygame.sprite.Sprite.__init__(self)
+		# 플레이어 오브젝트 스프라이트 목록을 저장하는 데 사용됩니다.
+		self.image = []
+		for i in range(len(player_rect)):
+			self.image.append(plane_img.subsurface(player_rect[i]).convert_alpha())
+		# 그림의 사각형을 초기화하십시오.
+		self.rect = player_rect[0]
+		# 사각형의 왼쪽 위 모서리 좌표를 초기화합니다.
+		self.rect.topleft = init_pos
+		# 초기 플레이어 속도, 여기에 특정 값이 있습니다.
+		self.speed = 8
+		# 플레이어의 항공기에 의해 발사 된 총알의 컬렉션
+		self.bullets = pygame.sprite.Group()
+		# 플레이어 마법사 그림 색인
+		self.img_index = 0
+		# 플레이어가 맞았는지 여부
+		self.is_hit = False                             
 	
 	# 플레이어가 총알을 발사했을때의 이벤트
-    def shoot(self, bullet_img):
-        bullet = Bullet(bullet_img, self.rect.midtop)
-        self.bullets.add(bullet)
+	def shoot(self, bullet_img):
+		bullet = Bullet(bullet_img, self.rect.midtop)
+		self.bullets.add(bullet)
 	
 	#플레이어가 상 하 좌 우 움직이게 되면 어느정도 이동하는지 계산하는 부분
-    def moveUp(self):
-        if self.rect.top <= 0:
-            self.rect.top = 0
-        else:
-            self.rect.top -= self.speed
+	def moveUp(self):
+		if self.rect.top <= 0:
+			self.rect.top = 0
+		else:
+			self.rect.top -= self.speed
+		
+	def moveDown(self):
+		if self.rect.top >= SCREEN_HEIGHT - self.rect.height:
+			self.rect.top = SCREEN_HEIGHT - self.rect.height
+		else:
+			self.rect.top += self.speed
 
-    def moveDown(self):
-        if self.rect.top >= SCREEN_HEIGHT - self.rect.height:
-            self.rect.top = SCREEN_HEIGHT - self.rect.height
-        else:
-            self.rect.top += self.speed
-
-    def moveLeft(self):
-        if self.rect.left <= 0:
-            self.rect.left = 0
-        else:
-            self.rect.left -= self.speed
-
-    def moveRight(self):
-        if self.rect.left >= SCREEN_WIDTH - self.rect.width:
-            self.rect.left = SCREEN_WIDTH - self.rect.width
-        else:
-            self.rect.left += self.speed
+	def moveLeft(self):
+		if self.rect.left <= 0:
+			self.rect.left = 0
+		else:
+			self.rect.left -= self.speed
+	
+	def moveRight(self):
+		if self.rect.left >= SCREEN_WIDTH - self.rect.width:
+			self.rect.left = SCREEN_WIDTH - self.rect.width
+		else:
+			self.rect.left += self.speed
 
 # 적 클래스
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, enemy_img, enemy_down_imgs, init_pos):
+	def __init__(self, enemy_img, enemy_down_imgs, init_pos):
 	# 적에 대한 기본 값 설정
-       pygame.sprite.Sprite.__init__(self)
-       self.image = enemy_img
-       self.rect = self.image.get_rect()
-       self.rect.topleft = init_pos
-       self.down_imgs = enemy_down_imgs
-       self.speed = 2
-       self.down_index = 0
-       self.bullets = pygame.sprite.Group()
+		pygame.sprite.Sprite.__init__(self)
+		self.image = enemy_img
+		self.rect = self.image.get_rect()
+		self.rect.topleft = init_pos
+		self.down_imgs = enemy_down_imgs
+		self.speed = 2
+		self.down_index = 0
+		self.bullets = pygame.sprite.Group()
 	   
 	# 플레이어가 총알을 발사했을때의 이벤트
-    def shoot(self, bullet_img):
-        bullet = Bullet(bullet_img, self.rect.midbottom)
-        self.bullets.add(bullet)
+	def shoot(self, bullet_img):
+		bullet = Bullet(bullet_img, self.rect.midbottom)
+		self.bullets.add(bullet)
 	
 	#적의 움직임 이벤트
-    def move(self):
-        self.rect.top += self.speed
+	def move(self):
+		self.rect.top += self.speed
+
+
+class Menu(pygame.sprite.Sprite):
+	def __init__(self, background, screen):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = background
+		self.menus = []
+		self.menus.append("Start")
+		self.menus.append("Exit")
+		self.focused = 0
+		self.font = pygame.font.Font(None, 48)
+		self.item = list()
+		self.item_rect = list()
+		self.offsety = 24
+		self.screen = screen
+		self.screen.blit(self.image, (0, 0))
+		print("menu init")
+				
+	def select(self, key):
+		print("waiting event")
+		if key == K_UP or key == K_w:
+			print("Up")
+			self.moveUp()
+			self.updateMenu()
+		if key == K_s or key == K_DOWN:
+			print("Down")
+			self.moveDown()
+			self.updateMenu()
+		if key == K_RETURN:
+			print("Return")
+			if self.focused == self.menus.index("Start"):
+				return 1
+			elif self.focused == self.menus.index("Exit"):
+				return 2
+		print("no event")
+		return 0
+
+	def moveUp(self):
+		self.focused = (self.focused+1)%2
+
+	def moveDown(self):
+		self.focused = (self.focused-1)%2
+
+	def updateMenu(self):
+		print("updateMenu")
+		del self.item
+		del self.item_rect
+		self.item = list()
+		self.item_rect = list()
+		i = 0
+		while i<len(self.menus):
+			m = self.menus[i]
+			if self.focused == i:
+				text = self.font.render(m, True, (0,255,0))
+			else:
+				text = self.font.render(m, True, (0,0,255))
+			self.item.append(text)
+			i = i+1
+
+		for i in self.item:
+			rect = i.get_rect()
+			self.item_rect.append(rect)
+
+		for i in self.item_rect:
+			i.centerx = self.screen.get_rect().centerx
+			i.centery = self.screen.get_rect().centery +self.offsety
+			self.offsety += 48
+		self.offsety = 24
+		i = 0
+		while i < len(self.item):
+			self.screen.blit(self.item[i], self.item_rect[i])
+			i+=1
+		pygame.display.update()
--- mainGame.py
+++ mainGame.py
@@ -13,6 +13,7 @@ from gameRole import *
 import random
 
 
+
 # 게임초기화
 # pyGame 라이브러리 초기화
 pygame.init()
@@ -94,180 +95,196 @@ score = 0
 clock = pygame.time.Clock()
 
 running = True
+ismenu = 0
 
-while running:
-    # 게임의 최대 프레임 속도를 60으로 제어
-    clock.tick(60)
+#시작 화면 변수
+menu = Menu(background, screen)
+menu.updateMenu()	
 
-    # 사격 횟수 제어 및 총알 발사
-    if not player.is_hit:
-        if shoot_frequency % 15 == 0:
-		# 총알 발사 사운드
-            bullet_sound.play()
-		# bullet_img == 총알 이미지
-            player.shoot(bullet_img)
-        shoot_frequency += 1
-        if shoot_frequency >= 15:
-            shoot_frequency = 0
-
-    # 적 항공기 생성
-    if enemy_frequency % 50 == 0:
-	#랜덤한 위치의 적 항공기 좌표 생성 width만 임의 , height는 맨끝
-        enemy1_pos = [random.randint(0, SCREEN_WIDTH - enemy1_rect.width), 0]
-	# 적 class 생성 좌표, 이미지, 적이 침몰했을때 이미지
-        enemy1 = Enemy(enemy1_img, enemy1_down_imgs, enemy1_pos)
-	# 적 그룹에 추가
-        enemies1.add(enemy1)
-
-    if enemy_shoot_frequency % 35 == 0:
-        for enemy in enemies1:    
-            #(추가)
-            enemy.shoot(bullet_img)
-    enemy_shoot_frequency += 1
-    if enemy_shoot_frequency >= 35:
-        enemy_shoot_frequency = 0
-
-    # 글 머리 기호가 창 밖으로 나가면 삭제하십시오. (추가)
-    for enemy in enemies1:
-        for bullet in enemy.bullets:
-            bullet.enemy_move()
-            if bullet.rect.bottom < 0:
-                enemy.bullets.remove(bullet)
-            
-        
-    #적 항공기 개수 변수에 추가
-    enemy_frequency += 1
-    if enemy_frequency >= 100:
-        enemy_frequency = 0
-
-    # 글 머리 기호가 창 밖으로 나가면 삭제하십시오.
-    for bullet in player.bullets:
-        bullet.move()
-        if bullet.rect.bottom < 0:
-            player.bullets.remove(bullet)
-			
-	
-
-    # 적기가 창 범위를 벗어난 경우 이동하여 삭제합니다.
-    for enemy in enemies1:
-	# 적 항공기 이동
-        enemy.move()
-        
-        # 플레이어가 맞았는지 확인
-        if pygame.sprite.collide_circle(enemy, player):
-            enemies_down.add(enemy)
-	    # 적항공기 전체 삭제
-            enemies1.remove(enemy)
-	    # player hit 변수 변경
-            player.is_hit = True
-	    # 게임 오버 사운드 재생
-            game_over_sound.play()
-            break
-        #추가
-        hits = pygame.sprite.spritecollide(player, enemy.bullets, True)
-        if hits:
-            # 적항공기 전체 삭제
-            enemies1.remove(enemy)
-            # player hit 변수 변경
-            player.is_hit = True
-            # 게임 오버 사운드 재생
-            game_over_sound.play()
-            break
-	# 스크린을 벗어났을경우 해당 적 삭제
-        if enemy.rect.top > SCREEN_HEIGHT:
-            enemies1.remove(enemy)	
-               
-
-    # 적의 비행기에 의해 맞을 것입니다 개체는 적군의 파괴에 추가, 난파 애니메이션을 렌더링하는 데 사용됩니다
-    enemies1_down = pygame.sprite.groupcollide(enemies1, player.bullets, 1, 1)
-    for enemy_down in enemies1_down:
-	# 격파한 적 항공기 목록에 추가		
-        enemies_down.add(enemy_down)
-
-    # 배경 그리기
-    screen.fill(0)
-    screen.blit(background, (0, 0))
-
-    # 플레이어 비행기를 그립니다.
-    if not player.is_hit:
-        screen.blit(player.image[player.img_index], player.rect)
-        # 항공기가 애니메이션 효과를 낼 수 있도록 그림 색인을 변경하십시오
-        player.img_index = shoot_frequency // 8
-    else:
-        player.img_index = player_down_index // 8
-        screen.blit(player.image[player.img_index], player.rect)
-        player_down_index += 1
-        if player_down_index > 47:
-            running = False
-
-    # 잔해 애니메이션 그리기
-    for enemy_down in enemies_down:
-        if enemy_down.down_index == 0:
-            enemy1_down_sound.play()
-        if enemy_down.down_index > 7:
-            enemies_down.remove(enemy_down)
-            score += 1000
-            continue
-        screen.blit(enemy_down.down_imgs[enemy_down.down_index // 2], enemy_down.rect)
-        enemy_down.down_index += 1
-
-    # 총알과 적기 그리기
-    player.bullets.draw(screen)
-    for enemy in enemies1:
-        enemy.bullets.draw(screen)
-    enemies1.draw(screen)
-
-    # 점수 뽑기
-    score_font = pygame.font.Font(None, 36)
-    score_text = score_font.render(str(score), True, (128, 128, 128))
-    text_rect = score_text.get_rect()
-    text_rect.topleft = [10, 10]
-    screen.blit(score_text, text_rect)
-
-    # 화면 업데이트
-    pygame.display.update()
-    
-    # 나가기 이벤트
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-
-    # 사용자가 죽었을 경우 자동 나가기 실행
-    #if player.is_hit :
-        #pygame.quit()
-        #exit()
-            
-    # 키보드 이벤트 가져오기
-    key_pressed = pygame.key.get_pressed()
-    # 플레이어가 죽지 않았을 때 키보드 키에 따라 플레이어 이동
-    if not player.is_hit:
-        if key_pressed[K_w] or key_pressed[K_UP]:
-            player.moveUp()
-        if key_pressed[K_s] or key_pressed[K_DOWN]:
-            player.moveDown()
-        if key_pressed[K_a] or key_pressed[K_LEFT]:
-            player.moveLeft()
-        if key_pressed[K_d] or key_pressed[K_RIGHT]:
-            player.moveRight()
-
-
-#게임내에 표시 되는 점수 등 location 설정
-font = pygame.font.Font(None, 48)
-text = font.render('Score: '+ str(score) + ' Exit : q', True, (255, 0, 0))
-text_rect = text.get_rect()
-text_rect.centerx = screen.get_rect().centerx
-text_rect.centery = screen.get_rect().centery + 24
-screen.blit(game_over, (0, 0))
-screen.blit(text, text_rect)
+# 게임의 최대 프레임 속도를 60으로 제어
+clock.tick(60)
+def endPage():
+	#게임내에 표시 되는 점수 등 location 설정
+	font = pygame.font.Font(None, 48)
+	text = font.render('Score: '+ str(score) + ' Exit : q', True, (255, 0, 0))
+	text_rect = text.get_rect()
+	text_rect.centerx = screen.get_rect().centerx
+	text_rect.centery = screen.get_rect().centery + 24
+	screen.blit(game_over, (0, 0))
+	screen.blit(text, text_rect)
     
 while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        key_pressed = pygame.key.get_pressed()
-        if key_pressed[K_q]:
-            pygame.quit()
-            exit()
-    pygame.display.update()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			exit()
+		key_pressed = pygame.key.get_pressed()
+		if key_pressed[K_q]:
+			pygame.quit()
+			exit()
+
+		if ismenu == 0:
+			if event.type == KEYDOWN:
+				ismenu = menu.select(event.key)
+		elif ismenu == 1:
+			running == True
+		elif ismenu == 2:
+			pygame.quit()
+			exit()
+		else:
+
+			while running:
+				# 사격 횟수 제어 및 총알 발사
+				clock.tick(60)
+				if not player.is_hit:
+					if shoot_frequency % 15 == 0:
+						# 총알 발사 사운드
+						bullet_sound.play()
+					# bullet_img == 총알 이미지
+						player.shoot(bullet_img)
+					shoot_frequency += 1
+					if shoot_frequency >= 15:
+						shoot_frequency = 0
+
+				# 적 항공기 생성
+				if enemy_frequency % 50 == 0:
+					#랜덤한 위치의 적 항공기 좌표 생성 width만 임의 , height는 맨끝
+					enemy1_pos = [random.randint(0, SCREEN_WIDTH - enemy1_rect.width), 0]
+					# 적 class 생성 좌표, 이미지, 적이 침몰했을때 이미지
+					enemy1 = Enemy(enemy1_img, enemy1_down_imgs, enemy1_pos)
+					# 적 그룹에 추가
+					enemies1.add(enemy1)
+
+				if enemy_shoot_frequency % 35 == 0:
+					for enemy in enemies1:    
+						#(추가)
+						enemy.shoot(bullet_img)
+				enemy_shoot_frequency += 1
+				if enemy_shoot_frequency >= 35:
+					enemy_shoot_frequency = 0
+
+				# 글 머리 기호가 창 밖으로 나가면 삭제하십시오. (추가)
+				for enemy in enemies1:
+					for bullet in enemy.bullets:
+						bullet.enemy_move()
+						if bullet.rect.bottom < 0:
+							enemy.bullets.remove(bullet)
+									
+							
+				#적 항공기 개수 변수에 추가
+				enemy_frequency += 1
+				if enemy_frequency >= 100:
+					enemy_frequency = 0
+
+				# 글 머리 기호가 창 밖으로 나가면 삭제하십시오.
+				for bullet in player.bullets:
+					bullet.move()
+					if bullet.rect.bottom < 0:
+						player.bullets.remove(bullet)
+
+				# 적기가 창 범위를 벗어난 경우 이동하여 삭제합니다.
+				for enemy in enemies1:
+				# 적 항공기 이동
+					enemy.move()
+
+					# 플레이어가 맞았는지 확인
+					if pygame.sprite.collide_circle(enemy, player):
+						enemies_down.add(enemy)
+						# 적항공기 전체 삭제
+						enemies1.remove(enemy)
+						# player hit 변수 변경
+						player.is_hit = True
+						# 게임 오버 사운드 재생
+						game_over_sound.play()
+						break
+					#추가
+					hits = pygame.sprite.spritecollide(player, enemy.bullets, True)
+					if hits:
+						# 적항공기 전체 삭제
+						enemies1.remove(enemy)
+						# player hit 변수 변경
+						player.is_hit = True
+						# 게임 오버 사운드 재생
+						game_over_sound.play()
+						break
+					# 스크린을 벗어났을경우 해당 적 삭제
+					if enemy.rect.top > SCREEN_HEIGHT:
+						enemies1.remove(enemy)	
+
+				# 적의 비행기에 의해 맞을 것입니다 개체는 적군의 파괴에 추가, 난파 애니메이션을 렌더링하는 데 사용됩니다
+				enemies1_down = pygame.sprite.groupcollide(enemies1, player.bullets, 1, 1)
+				for enemy_down in enemies1_down:
+				# 격파한 적 항공기 목록에 추가		
+					enemies_down.add(enemy_down)
+
+				# 배경 그리기
+				screen.fill(0)
+				screen.blit(background, (0, 0))
+
+				# 플레이어 비행기를 그립니다.
+				if not player.is_hit:
+					screen.blit(player.image[player.img_index], player.rect)
+					# 항공기가 애니메이션 효과를 낼 수 있도록 그림 색인을 변경하십시오
+					player.img_index = shoot_frequency // 8
+				else:
+					player.img_index = player_down_index // 8
+					screen.blit(player.image[player.img_index], player.rect)
+					player_down_index += 1
+					if player_down_index > 47:
+						running = False
+
+				# 잔해 애니메이션 그리기
+				for enemy_down in enemies_down:
+					if enemy_down.down_index == 0:
+						enemy1_down_sound.play()
+					if enemy_down.down_index > 7:
+						enemies_down.remove(enemy_down)
+						score += 1000
+						continue
+					screen.blit(enemy_down.down_imgs[enemy_down.down_index // 2], enemy_down.rect)
+					enemy_down.down_index += 1
+
+				# 총알과 적기 그리기
+				player.bullets.draw(screen)
+				for enemy in enemies1:
+					enemy.bullets.draw(screen)
+				enemies1.draw(screen)
+
+				# 점수 뽑기
+				score_font = pygame.font.Font(None, 36)
+				score_text = score_font.render(str(score), True, (128, 128, 128))
+				text_rect = score_text.get_rect()
+				text_rect.topleft = [10, 10]
+				screen.blit(score_text, text_rect)
+
+				# 화면 업데이트
+				pygame.display.update()
+				
+				# 나가기 이벤트
+				for event in pygame.event.get():
+					if event.type == pygame.QUIT:
+						pygame.quit()
+						exit()
+
+				# 사용자가 죽었을 경우 자동 나가기 실행
+				#if player.is_hit :
+						#pygame.quit()
+						#exit()
+								
+				# 키보드 이벤트 가져오기
+				key_pressed = pygame.key.get_pressed()
+				# 플레이어가 죽지 않았을 때 키보드 키에 따라 플레이어 이동
+				if not player.is_hit:
+					if key_pressed[K_w] or key_pressed[K_UP]:
+						player.moveUp()
+					if key_pressed[K_s] or key_pressed[K_DOWN]:
+						player.moveDown()
+					if key_pressed[K_a] or key_pressed[K_LEFT]:
+						player.moveLeft()
+					if key_pressed[K_d] or key_pressed[K_RIGHT]:
+						player.moveRight()
+			endPage()
+
+
+
+	pygame.display.update()
